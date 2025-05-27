fa7e92c2e6f6005e65ca9d552da0baeb00b8ee1e
AmitNachum <163768953+AmitNachum@users.noreply.github.com>
2025-05-27 22:00:40 +0300

    Delete globals.c

--- globals.c
+++
@@ -1,9 +0,0 @@
-#include "../Q_1/atoms.h"
-
-unsigned int hydrogen = 0;
-unsigned int oxygen = 0;
-unsigned int carbon = 0;
-unsigned int water = 0;
-unsigned int carbon_dioxide = 0;
-unsigned int alcohol = 0;
-unsigned int glucose = 0;
